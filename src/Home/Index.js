8c9d6707249ba5acd4cfd5a7846a7047abd002f5
Yash Zawar <yash.zawar@ril.com>
2024-03-10 18:48:25 +0530

    changes in logout uri

--- src/Home/Index.js
+++ src/Home/Index.js
@@ -37,7 +37,7 @@ const Home = () => {
 					<button
 						type="button"
 						onClick={() => {
-							void signOut(baseUrl);
+							void signOut(`${baseUrl}/logto-app`);
 						}}
 					>
 						Sign out
